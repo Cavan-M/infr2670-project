b903c5a61f6ce35b290d00b85e298d0508d27a90
Cavan-M <cavan8553@gmail.com>
2022-11-09 05:12:08 +0000

    first update

--- base.py
+++ base.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def mainFunction():
-	return "TEST"
+	return "Uh Oh! Looks like something went wrong..."
+
+@app.route('/api/<query>')
+def apiFunction(query):
+	q = query
+	return q
 
 if __name__ == '__main__':
 	app.run(debug=True, host="0.0.0.0")
