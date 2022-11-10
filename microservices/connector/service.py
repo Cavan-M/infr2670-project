6a4dae29ec437737553364b3f6ad2769d84c1a62
Cavan-M <cavan8553@gmail.com>
2022-11-10 19:34:22 +0000

    added connector

---
+++ microservices/connector/service.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def mainFunction():
+	return "Uh Oh! Looks like something went wrong..."
+
+
+@app.route('/api/<query>')
+def microservice(query):
+	q = query.split('%')
+	endpoint = "http://" + q[0] + '/api/' + q[1]
+		
+	r = requests.get(endpoint)
+	return r.text
+
+
+if __name__ == '__main__':
+	app.run(debug=True, host="0.0.0.0", port=8080)
